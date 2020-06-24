6d36ab6d028b818a0ccd34de1f5332be4e070422
kbatista <kbatista@uolinc.com>
2020-06-24 20:26:33 -0300

    new setup

--- app/services/setup_service.py
+++ app/services/setup_service.py
@@ -50,12 +50,14 @@ def find_setup(ticker):
         and stock_2_price_close <= stock_0_ema_9
         """)
     politic.add('Possível Setup 9.2 de Compra',
-                """ stock_0_price_close < stock_1_price_low
+                """ stock_0_price_close < stock_1_price_close
         and stock_0_price_close < stock_0_price_open
         and stock_0_price_close >= stock_0_ema_9
         and stock_0_ema_9 > stock_1_ema_9
         and stock_1_ema_9 > stock_2_ema_9
         and stock_2_ema_9 > stock_3_ema_9
+        and stock_3_ema_9 > stock_4_ema_9
+        and stock_4_ema_9 > stock_5_ema_9
         """)
     politic.add('Possível Setup 9.3 de Compra',
                 """ stock_0_price_close < stock_2_price_close
@@ -65,7 +67,22 @@ def find_setup(ticker):
         and stock_0_ema_9 > stock_1_ema_9
         and stock_1_ema_9 > stock_2_ema_9
         and stock_2_ema_9 > stock_3_ema_9
+        and stock_3_ema_9 > stock_4_ema_9
+        and stock_4_ema_9 > stock_5_ema_9
         """)
+
+    politic.add('Possível PC de Compra',
+            """ stock_0_price_low <= stock_0_ema_21
+    and stock_0_price_close <= stock_1_price_close
+    and stock_1_price_close <= stock_2_price_close
+    and stock_2_price_close <= stock_3_price_close
+    and stock_0_price_close >= stock_0_ema_21
+    and stock_1_price_close >= stock_0_ema_21
+    and stock_2_price_close >= stock_0_ema_21
+    and stock_3_price_close >= stock_0_ema_21
+    and stock_4_price_close >= stock_0_ema_21
+    and stock_5_price_close >= stock_0_ema_21
+    """)
 
     for key, value in enumerate(list_stock):
         stocks[f'stock_{key}_price_close'] = float(value.price_close)
