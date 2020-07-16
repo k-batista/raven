3cb3a700b307ab119ac307233ee6ce58f9d7395a
kbatista <kbatista@uolinc.com>
2020-07-15 23:27:29 -0300

    setups

--- app/services/setups.py
+++ app/services/setups.py
@@ -16,59 +16,72 @@ setup_factory.add('Possível Setup 9.1 de Compra',
 
 setup_factory.add('Possível PC de Compra',
                   """ stock_0_price_low <= stock_0_ema_21
-                and (stock_0_price_close <= stock_1_price_close
-                    or stock_0_price_close <= stock_1_price_open)
-                and (stock_1_price_close <= stock_2_price_close
-                    or stock_1_price_close <= stock_2_price_open)
+                and stock_0_price_close <= stock_1_price_close
+                and stock_1_price_close <= stock_2_price_close
                 and (stock_0_price_close >= stock_0_ema_21
                     or stock_0_price_open >= stock_0_ema_21)
                 and stock_1_price_close >= stock_0_ema_21
                 and stock_2_price_close >= stock_0_ema_21
                 and stock_3_price_close >= stock_0_ema_21
                 and stock_4_price_close >= stock_0_ema_21
-                and stock_5_price_close >= stock_0_ema_21
                 """)
 
 setup_factory.add('Possível Setup 9.3 de Compra',
-                  """ stock_0_price_close < stock_1_price_close
+                  """ stock_0_price_open < stock_1_price_open
                 and stock_0_price_close < stock_2_price_close
                 and stock_1_price_close < stock_2_price_close
                 and stock_0_price_close >= stock_0_ema_9
                 and stock_1_price_close >= stock_0_ema_9
+                and (stock_2_price_high > stock_3_price_high
+                    or stock_3_price_high > stock_3_price_close)
+                and (stock_3_price_high > stock_4_price_high
+                    or stock_4_price_high > stock_4_price_close)
+                and (stock_4_price_high > stock_5_price_high
+                    or stock_4_price_high > stock_5_price_close)
                 and stock_0_ema_9 >= stock_1_ema_9
                 and stock_1_ema_9 >= stock_2_ema_9
                 and stock_2_ema_9 >= stock_3_ema_9
                 and stock_3_ema_9 >= stock_4_ema_9
-                and stock_4_ema_9 >= stock_5_ema_9
                 """)
 
 setup_factory.add('Possível Setup 9.2 de Compra',
-                  """ (stock_0_price_close < stock_1_price_close
-                      or stock_0_price_close < stock_1_price_open)
-                and stock_0_price_close < stock_0_price_open
+                  """ stock_0_price_close < stock_1_price_low
+                and (stock_1_price_high > stock_2_price_high
+                    or stock_1_price_high > stock_2_price_close)
+                and (stock_2_price_high > stock_3_price_high
+                    or stock_3_price_high > stock_3_price_close)
+                and (stock_3_price_high > stock_4_price_high
+                    or stock_4_price_high > stock_4_price_close)
+                and (stock_4_price_high > stock_5_price_high
+                    or stock_4_price_high > stock_5_price_close)
                 and stock_0_ema_9 > stock_1_ema_9
+                and stock_1_ema_9 > stock_2_ema_9
+                and stock_2_ema_9 > stock_3_ema_9
+                and stock_3_ema_9 > stock_4_ema_9
+                """)
+
+setup_factory.add('Possível Setup 9.1 de Venda',
+                  """ stock_0_price_close <= stock_0_ema_9
+                and stock_0_price_close < stock_0_price_open
+                and stock_1_price_close > stock_1_ema_9
+                and stock_2_price_close > stock_2_ema_9
+                and stock_3_price_close > stock_3_ema_9
+                and stock_4_price_close > stock_4_ema_9
                 and stock_1_ema_9 > stock_2_ema_9
                 and stock_2_ema_9 > stock_3_ema_9
                 and stock_3_ema_9 > stock_4_ema_9
                 and stock_4_ema_9 > stock_5_ema_9
                 """)
 
-setup_factory.add('Possível Setup 9.1 de Venda',
-                  """ stock_0_price_close < stock_0_ema_9
-                and stock_0_price_close <= stock_0_price_open
-                and stock_1_price_close >= stock_1_ema_9
-                and stock_2_price_close >= stock_2_ema_9
-                and stock_3_price_close >= stock_3_ema_9
-                and stock_1_ema_9 >= stock_2_ema_9
-                and stock_2_ema_9 >= stock_3_ema_9
-                and stock_3_ema_9 >= stock_4_ema_9
-                """)
-
 setup_factory.add('Possível Setup 9.3 de Venda',
-                  """ stock_0_price_close > stock_2_price_close
+                  """ stock_0_price_close > stock_1_price_close
+                and stock_0_price_close > stock_2_price_close
                 and stock_1_price_close > stock_2_price_close
                 and stock_0_price_close < stock_0_ema_9
                 and stock_1_price_close < stock_0_ema_9
+                and stock_2_price_high < stock_3_price_high
+                and stock_3_price_high < stock_4_price_high
+                and stock_4_price_high < stock_5_price_high
                 and stock_0_ema_9 < stock_1_ema_9
                 and stock_1_ema_9 < stock_2_ema_9
                 and stock_2_ema_9 < stock_3_ema_9
@@ -77,14 +90,17 @@ setup_factory.add('Possível Setup 9.3 de Venda',
                 """)
 
 setup_factory.add('Possível Setup 9.2 de Venda',
-                  """ (stock_0_price_close > stock_1_price_close
-                      or stock_0_price_close > stock_1_price_open)
-                and stock_0_price_close > stock_0_price_open
+                  """ stock_0_price_close > stock_1_price_low
+                and stock_1_price_high < stock_2_price_high
+                and stock_2_price_high < stock_3_price_high
+                and stock_3_price_high < stock_4_price_high
+                and stock_4_price_high < stock_5_price_high
                 and stock_0_ema_9 < stock_1_ema_9
                 and stock_1_ema_9 < stock_2_ema_9
                 and stock_2_ema_9 < stock_3_ema_9
                 and stock_3_ema_9 < stock_4_ema_9
                 and stock_4_ema_9 < stock_5_ema_9
+                and stock_5_ema_9 < stock_6_ema_9
                 """)
 
 setup_factory.add('Possível PC de Venda',
